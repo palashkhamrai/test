2033370dc8946218a8839af925021ce4c8898707
palashkhamrai <46481977+palashkhamrai@users.noreply.github.com>
2021-11-22 23:47:48 +0530

    Create palash.sh

---
+++ palash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export  PATH='/sbin:/bin:/usr/sbin:/usr/bin:~/.local/bin/'
+DIR="test"
+if [ -d "$DIR" ]; then
+  # Take action if $DIR exists. #
+  echo "${DIR} Present"
+  cd $DIR
+  git pull https://github.com/palashkhamrai/$DIR.git
+
+else 
+  echo "Directory $DIR Not Present"
+  git clone https://github.com/palashkhamrai/$DIR.git
+  cd $DIR
+fi
+
+echo "Hello from palash12234567890"
